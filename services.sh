81885c68ec822a7ede1aafad89a6297946b747da
Sales Macedo <salesmacedoeducacional@gmail.com>
2024-11-24 00:07:22 -0300

    ultimo commit

--- services.sh
+++ services.sh
@@ -1,3 +1,4 @@
 gcloud services enable run.googleapis.com
 gcloud services enable cloudbuild.googleapis.com
 gcloud services enable artifactregistry.googleapis.com
+gcloud services enable service1 service2 --project INSTALIKE-BACK
